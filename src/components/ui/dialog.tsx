4bafb426b2aca26e9f272162400af0f7cd8d275c
yousef osama <yousefosama654@gmail.com>
2023-11-02 18:02:41 +0200

    resolve syntax error

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-"../../
-import { cn } from lib/utils";
+import { cn } from "../../lib/utils";
 
 const Dialog = DialogPrimitive.Root;
 
