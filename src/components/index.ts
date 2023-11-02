6e2031c14b11c5d68bf8975f868b2b371698b01f
marwan995 <mroansamy99@gamil.com>
2023-11-03 01:25:29 +0200

    AUTH<username>

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,6 @@ import { UsernameSuggestion } from "./UsernameSuggestion/UsernameSuggestion";
 import { Button } from "./ui/button";
 import { PopUpContainer } from "./PopUpContainer/PopUpContainer";
 import { SignUpSteps } from "./SignUpSteps/SignUpSteps";
-import TextInput from "./TextInput/TextInput";
+import {TextInput} from "./TextInput/TextInput";
 
 export { NotificationAllow, Button, PopUpContainer, SignUpSteps, TextInput,UsernameSuggestion };
