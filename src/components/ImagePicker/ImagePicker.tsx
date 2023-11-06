c950868bd7fe988f5863f9b374d9fa4e6951fd21
MohamedAtef308 <mimo3082002@gmail.com>
2023-11-06 22:56:37 +0200

    AUTH<textinput>

--- src/components/ImagePicker/ImagePicker.tsx
+++ src/components/ImagePicker/ImagePicker.tsx
@@ -15,15 +15,12 @@ export const ImagePicker = ({
   iconSize = "24",
   imageClassName,
   className,
-  defaultImage = "",
+  defaultImage = "https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
   alt = "Upladed Image",
-  ...props
 }: ImagePickerProps) => {
   const [profilePic, setProfilePic] = useState<
     string | ArrayBuffer | null | undefined
-  >(
-    "https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
-  );
+  >(defaultImage);
   const inputFileRef = useRef<HTMLInputElement | null>(null);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
--- src/components/SignUpProfile/SignUpProfile.tsx
+++ src/components/SignUpProfile/SignUpProfile.tsx
@@ -1,17 +1,9 @@
-import { FieldValues, useForm } from "react-hook-form";
 import { ImagePicker } from "../ImagePicker/ImagePicker";
 import { PopUpContainer } from "../PopUpContainer/PopUpContainer";
 import { Button } from "../ui/button";
-import { useRef } from "react";
 
 export const SignUpProfile = () => {
-  const formRef = useRef();
-
-  const form = useForm();
-
-  const onSubmit = (data: FieldValues) => {
-    // NEEDED: send photo to backend
-  };
+  // NEEDED: go to the next step
 
   return (
     <PopUpContainer show={true} showLogo={true} className="justify-between">
@@ -22,15 +14,9 @@ export const SignUpProfile = () => {
             Have a favorite selfie? Upload it now.
           </h5>
         </div>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full h-[320px] flex justify-center items-center"
-        >
-          <ImagePicker
-            {...form.register}
-            defaultImage="https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
-          />
-        </form>
+        <div className="w-full h-[320px] flex justify-center items-center">
+          <ImagePicker defaultImage="https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" />
+        </div>
       </div>
       <Button variant="outline" size="full" className="h-[50px] font-bold my-1">
         Skip for now
