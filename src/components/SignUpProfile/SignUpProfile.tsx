9373b7aab4efef1662287426257d28fb1b1c5816
MohamedAtef308 <mimo3082002@gmail.com>
2023-11-06 22:53:53 +0200

    AUTH<ImagePicker done>

--- src/components/SignUpProfile/SignUpProfile.tsx
+++ src/components/SignUpProfile/SignUpProfile.tsx
@@ -1,7 +1,18 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { ImagePicker } from "../ImagePicker/ImagePicker";
 import { PopUpContainer } from "../PopUpContainer/PopUpContainer";
 import { Button } from "../ui/button";
+import { useRef } from "react";
 
 export const SignUpProfile = () => {
+  const formRef = useRef();
+
+  const form = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    // NEEDED: send photo to backend
+  };
+
   return (
     <PopUpContainer show={true} showLogo={true} className="justify-between">
       <div className="w-full flex flex-col justify-start items-start">
@@ -11,9 +22,15 @@ export const SignUpProfile = () => {
             Have a favorite selfie? Upload it now.
           </h5>
         </div>
-        <div className="w-full h-[320px] flex justify-center items-center">
-          picker here
-        </div>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full h-[320px] flex justify-center items-center"
+        >
+          <ImagePicker
+            {...form.register}
+            defaultImage="https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+          />
+        </form>
       </div>
       <Button variant="outline" size="full" className="h-[50px] font-bold my-1">
         Skip for now
