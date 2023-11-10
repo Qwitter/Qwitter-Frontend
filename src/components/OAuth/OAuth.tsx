3aa60399bb07024088fa6bb50788a718dd5bd8d2
Sofa <sofa5060@gmail.com>
2023-11-10 11:53:57 +0200

    AUTH<oAuth>

--- src/components/OAuth/OAuth.tsx
+++ src/components/OAuth/OAuth.tsx
@@ -3,17 +3,16 @@ import { default as BirthDayComponent } from "../BirthDay/BirthDay";
 import { type BirthDay } from "@/models/BirthDay";
 import { HeaderButton } from "@/models/PopUpModel";
 import { PopUpContainer } from "../PopUpContainer/PopUpContainer";
-import EmailVerification from "../EmailVerification/EmailVerification";
 import { useLocation } from "react-router-dom";
 import { User } from "@/models/User";
 import { useContext } from "react";
 import { UserContext } from "@/contexts/UserContextProvider";
+import { Step2 } from "../SignUpSteps/Step2";
 
 const OAuth = () => {
   const [step, setStep] = useState(0);
   const [birthDay, setBirthDay] = useState<BirthDay | null>(null);
   const location = useLocation();
-  const { user, token } = location.state as { user: User; token: string };
   const { saveUser } = useContext(UserContext);
 
   const nextStep = () => {
@@ -25,9 +24,11 @@ const OAuth = () => {
     setStep(step - 1);
   };
 
-  const signup = () => {
+  const signUp = () => {
+    if (!location.state) return;
+    const { user, token } = location.state as { user: User; token: string };
     saveUser(user, token);
-  }
+  };
 
   const steps = [
     <BirthDayComponent
@@ -35,19 +36,14 @@ const OAuth = () => {
       setBirthDay={setBirthDay}
       birthDay={birthDay}
     />,
-    <EmailVerification
-      email="sofa5060@gmail.com"
-      verificationType="passwordReset"
-      onSuccess={nextStep}
-      onFail={prevStep}
-    />,
+    <Step2 nextStep={signUp} />,
   ];
 
   return (
     <PopUpContainer
       show
       showLogo={step === 0}
-      title={step === 1 ? `Step ${step + 1} of ${5}` : ""}
+      title={step === 1 ? `Step ${step + 1} of 5` : ""}
       className="px-10 h-full justify-start"
       headerButton={step > 0 ? HeaderButton.back : HeaderButton.none}
       headerFunction={prevStep}
