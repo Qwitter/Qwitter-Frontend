65d866a040a7ec796982e31cf63084dc577bdd38
Sofa <sofa5060@gmail.com>
2023-11-10 21:52:54 +0200

    AUTH<oAuth>

--- src/mocks/OAuthRegisterUserWorker/OAuthRegisterUserWorker.ts
+++ src/mocks/OAuthRegisterUserWorker/OAuthRegisterUserWorker.ts
@@ -18,8 +18,23 @@ export const OAuthRegisterUserWorker: ResponseResolver = async ({
 
   return HttpResponse.json(
     {
-      message:
-        "user is created successfully for you with birthDate: " + birthDate,
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZmNTgyOGMyLTBkYzUtNDNlMS1hZmQzLWFhODI3ZmQxODRiYSIsImlhdCI6MTY5OTI3MjE0MiwiZXhwIjoxNzA3MDQ4MTQyfQ.s4Bgs8RJr9U242CdG9cJyiVK6N7_VAVw9mziMdAkFrM",
+      "data": {
+        "userName": "johndoe123",
+        "name": "Ahmed Zahran",
+        "birthDate": birthDate,
+        "url": "www.johndoe.com",
+        "description": "Hi i am john doe",
+        "protected": false,
+        "verified": true,
+        "followersCount": 12312,
+        "followingCount": 1932,
+        "createdAt": "2020-03-09T22:18:26.625Z",
+        "profileBannerUrl": "https://notdeterminedyet.com/profile_banners/819797/1348102824",
+        "profileImageUrl": "https://notdeterminedyet.com/profile_banners/819797/1348102824",
+        "email": "<string>",
+        "username": "<string>"
+      }
     },
     {
       status: 200,
