7700e915cab0eb3d08a1c6d8fd5751e113a86753
MohamedAtef308 <mimo3082002@gmail.com>
2023-11-11 11:26:00 +0200

    AUTH<signup step 4>

--- src/components/SignUpSteps/SignUpSteps.tsx
+++ src/components/SignUpSteps/SignUpSteps.tsx
@@ -10,6 +10,8 @@ import { z } from "zod";
 import { useNavigate } from "react-router-dom";
 import { registerNewUser } from "@/lib/utils";
 import { BirthDaySchema } from "@/models/BirthDay";
+import EmailVerification from "../EmailVerification/EmailVerification";
+import { RecaptchaPopUp } from "../RecaptchaPopUp/RecaptchaPopUp";
 
 /*
   NEEDED:
@@ -82,9 +84,14 @@ export const SignUpSteps = () => {
     />,
     <Step2 nextStep={nextStep} />,
     <Step3 nextStep={nextStep} resetStep={resetStep} userData={userData} />,
-    // <RecaptchaPopUp afterAuth={nextStep} />,
-    // < />,
-    <Step5 nextStep={() => { }} registerUser={registerUser} />,
+    <RecaptchaPopUp afterAuth={nextStep} />,
+    // <EmailVerification
+    //   email={userData?.email || ""}
+    //   onSuccess={nextStep}
+    //   onFail={resetStep}
+    //   verificationType="signUp"
+    // />,
+    <Step5 nextStep={() => {}} registerUser={registerUser} />,
   ];
 
   // holds which button (x, arrow or none) is shown
@@ -92,6 +99,8 @@ export const SignUpSteps = () => {
     HeaderButton.close,
     HeaderButton.back,
     HeaderButton.back,
+    HeaderButton.none,
+    // HeaderButton.back,
     HeaderButton.none,
   ];
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./mocks/MockServer.ts";
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-
       <App />
     </Router>
   </React.StrictMode>
