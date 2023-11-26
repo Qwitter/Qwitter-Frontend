5ce611b073adf5a8ce8274fd9b0abc0e47513437
Sofa <sofa5060@gmail.com>
2023-11-27 01:18:54 +0200

    TWEET<generalized image component & added liked>

--- src/components/TweetImagesViewer/TweetImagesViewer.tsx
+++ src/components/TweetImagesViewer/TweetImagesViewer.tsx
@@ -1,19 +1,38 @@
 import { cn } from "@/lib";
 import { Tweet } from "@/models/Tweet";
+import { HiOutlineXMark } from "react-icons/hi2";
 
-type TweetImagesViewerProps = {
+type TweetImagesViewerOnly = {
+  mode?: "view-only";
   images: Tweet["entities"]["media"];
+  removeAttachment?: (index: number) => void;
 };
 
-const TweetImagesViewer = ({ images }: TweetImagesViewerProps) => {
+type TweetImagesEditor = {
+  mode?: "edit";
+  images: Tweet["entities"]["media"];
+  removeAttachment: (index: number) => void;
+};
+
+type TweetImagesViewerProps = TweetImagesEditor | TweetImagesViewerOnly;
+
+const TweetImagesViewer = ({
+  images,
+  mode = "view-only",
+  removeAttachment,
+}: TweetImagesViewerProps) => {
   return (
     <div
       className={cn("mt-4 rounded-xl overflow-hidden w-full", {
         "max-h-[290px] pb-[290px] h-full": images.length > 1,
+        "max-h-[298px]": mode === "edit" && images.length === 4,
+        hidden: images.length === 0,
       })}
     >
       {images.length === 3 ? (
-        <div className="flex h-[290px] gap-0.5">
+        <div
+          className={cn("flex h-[290px] gap-0.5", { "gap-2": mode === "edit" })}
+        >
           <div
             className={cn("relative flex-1 h-full w-full overflow-hidden")}
             key={images[0].value}
@@ -21,11 +40,23 @@ const TweetImagesViewer = ({ images }: TweetImagesViewerProps) => {
             <img
               src={images[0].value}
               alt=""
-              className={cn("h-full absolute object-cover min-w-full")}
+              className={cn("h-full absolute object-cover min-w-full", {
+                "rounded-xl": mode === "edit",
+              })}
             />
+            {mode === "edit" && (
+              <HiOutlineXMark
+                className="tweet-image-remover-icon"
+                onClick={() => removeAttachment!(0)}
+              />
+            )}
           </div>
-          <div className="flex flex-col flex-1 gap-0.5">
-            {images.slice(1, 3).map((image) => (
+          <div
+            className={cn("flex flex-col flex-1 gap-0.5", {
+              "gap-2": mode === "edit",
+            })}
+          >
+            {images.slice(1, 3).map((image, i) => (
               <div
                 className="relative flex-1 h-full w-full overflow-hidden"
                 key={image.value}
@@ -33,8 +64,16 @@ const TweetImagesViewer = ({ images }: TweetImagesViewerProps) => {
                 <img
                   src={image.value}
                   alt=""
-                  className={cn("h-full absolute object-cover min-w-full")}
+                  className={cn("h-full absolute object-cover min-w-full", {
+                    "rounded-xl": mode === "edit",
+                  })}
                 />
+                {mode === "edit" && (
+                  <HiOutlineXMark
+                    className="tweet-image-remover-icon"
+                    onClick={() => removeAttachment!(i + 1)}
+                  />
+                )}
               </div>
             ))}
           </div>
@@ -46,9 +85,10 @@ const TweetImagesViewer = ({ images }: TweetImagesViewerProps) => {
               "h-full": images.length === 1,
               "h-0": images.length === 0,
               "h-[145px]": images.length >= 4,
+              "gap-2": mode === "edit",
             })}
           >
-            {images.slice(0, 2).map((image) => (
+            {images.slice(0, 2).map((image, i) => (
               <div
                 className={cn("relative flex-1 h-full w-full overflow-hidden")}
                 key={image.value}
@@ -58,13 +98,25 @@ const TweetImagesViewer = ({ images }: TweetImagesViewerProps) => {
                   alt=""
                   className={cn("h-full absolute object-cover min-w-full", {
                     static: images.length === 1,
+                    "rounded-xl": mode === "edit",
                   })}
                 />
+                {mode === "edit" && (
+                  <HiOutlineXMark
+                    className="tweet-image-remover-icon"
+                    onClick={() => removeAttachment!(i)}
+                  />
+                )}
               </div>
             ))}
           </div>
-          <div className={cn("flex h-[145px] mt-px gap-px", {"h-0": images.length <= 2})}>
-            {images.slice(2, 4).map((image) => (
+          <div
+            className={cn("flex h-[145px] mt-px gap-px", {
+              "h-0": images.length <= 2,
+              "gap-2 mt-2": mode === "edit",
+            })}
+          >
+            {images.slice(2, 4).map((image, i) => (
               <div
                 className="relative flex-1 h-full w-full overflow-hidden"
                 key={image.value}
@@ -74,8 +126,15 @@ const TweetImagesViewer = ({ images }: TweetImagesViewerProps) => {
                   alt=""
                   className={cn("h-full absolute object-cover min-w-full", {
                     static: images.length === 1,
+                    "rounded-xl": mode === "edit",
                   })}
                 />
+                {mode === "edit" && (
+                  <HiOutlineXMark
+                    className="tweet-image-remover-icon"
+                    onClick={() => removeAttachment!(i + 2)}
+                  />
+                )}
               </div>
             ))}
           </div>
--- src/components/TweetInteractionsButtons/TweetInteractionsButtons.tsx
+++ src/components/TweetInteractionsButtons/TweetInteractionsButtons.tsx
@@ -1,18 +1,50 @@
 import { Tweet } from "@/models/Tweet";
 import { BiRepost } from "react-icons/bi";
-import { AiOutlineHeart } from "react-icons/ai";
-// import { RiReplyLine } from "react-icons/ri";
-// import { BsBookmarkFill } from "react-icons/bs";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsBookmarkFill } from "react-icons/bs";
 import { BsBookmark } from "react-icons/bs";
 import { GoComment } from "react-icons/go";
 import { IoMdStats } from "react-icons/io";
-import { convertNumberToShortForm } from "@/lib/utils";
+import { cn, convertNumberToShortForm } from "@/lib/utils";
+import { useState } from "react";
 
 type TweetInteractionsButtonsProps = {
   tweet: Tweet;
 };
 
 const TweetInteractionsButtons = ({ tweet }: TweetInteractionsButtonsProps) => {
+  const [tweetClone, setTweetClone] = useState<Tweet>(tweet);
+
+  const likeTweet = () => {
+    setTweetClone((prev: Tweet) => ({
+      ...prev,
+      liked: true,
+      likesCount: prev.likesCount + 1,
+    }));
+  };
+
+  const unLikeTweet = () => {
+    setTweetClone((prev: Tweet) => ({
+      ...prev,
+      liked: false,
+      likesCount: prev.likesCount - 1,
+    }));
+  };
+
+  const bookmarkTweet = () => {
+    setTweetClone((prev: Tweet) => ({
+      ...prev,
+      bookmarked: true,
+    }));
+  };
+
+  const unBookmarkTweet = () => {
+    setTweetClone((prev: Tweet) => ({
+      ...prev,
+      bookmarked: false,
+    }));
+  };
+
   return (
     <div className="flex gap-4 mt-4 text-gray justify-between">
       <div className="tweet-icon-container group">
@@ -21,7 +53,7 @@ const TweetInteractionsButtons = ({ tweet }: TweetInteractionsButtonsProps) => {
           <GoComment className="tweet-icon group-hover:text-secondary" />
         </div>
         <span className="text-gray-500 transition-all group-hover:text-secondary">
-          {convertNumberToShortForm(tweet.replyCount)}
+          {convertNumberToShortForm(tweetClone.replyCount)}
         </span>
       </div>
       <div className="tweet-icon-container group">
@@ -30,16 +62,28 @@ const TweetInteractionsButtons = ({ tweet }: TweetInteractionsButtonsProps) => {
           <BiRepost className="tweet-icon group-hover:text-[#00ba7c]" />
         </div>
         <span className="text-gray-500 transition-all group-hover:text-[#00ba7c]">
-          {convertNumberToShortForm(tweet.retweetCount)}
+          {convertNumberToShortForm(tweetClone.retweetCount)}
         </span>
       </div>
-      <div className="tweet-icon-container group">
+      <div
+        className="tweet-icon-container group"
+        onClick={tweetClone.liked ? unLikeTweet : likeTweet}
+      >
         <div className="relative">
           <div className="tweet-icon-radius group-hover:bg-[#f91880]"></div>
-          <AiOutlineHeart className="tweet-icon group-hover:text-[#f91880]" />
+          {tweetClone.liked ? (
+            <AiFillHeart className="tweet-icon fill-[#f91880]" />
+          ) : (
+            <AiOutlineHeart className="tweet-icon group-hover:text-[#f91880]" />
+          )}
         </div>
-        <span className="text-gray-500 transition-all group-hover:text-[#f91880]">
-          {convertNumberToShortForm(tweet.likesCount)}
+        <span
+          className={cn(
+            "text-gray-500 transition-all group-hover:text-[#f91880]",
+            { "text-[#f91880]": tweetClone.liked }
+          )}
+        >
+          {convertNumberToShortForm(tweetClone.likesCount)}
         </span>
       </div>
       <div className="tweet-icon-container group">
@@ -51,9 +95,19 @@ const TweetInteractionsButtons = ({ tweet }: TweetInteractionsButtonsProps) => {
           {convertNumberToShortForm(13000)}
         </span>
       </div>
-      <div className="relative cursor-pointer group">
+      <div className="relative cursor-pointer group" onClick={tweetClone.bookmarked ? unBookmarkTweet : bookmarkTweet}>
         <div className="tweet-icon-radius group-hover:bg-secondary"></div>
-        <BsBookmark className="tweet-icon group-hover:text-secondary" />
+        {tweetClone.bookmarked ? (
+          <BsBookmarkFill
+            className="tweet-icon fill-secondary"
+            onClick={unBookmarkTweet}
+          />
+        ) : (
+          <BsBookmark
+            className="tweet-icon group-hover:text-secondary"
+            onClick={bookmarkTweet}
+          />
+        )}
       </div>
     </div>
   );
