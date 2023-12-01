2a52bf3d78cea182da05d1e96ff0b537ec02ae62
yousef osama <yousefosama654@gmail.com>
2023-12-01 15:41:21 +0200

    FOLLOW<edit login format>

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -10,82 +10,97 @@ import { useMutation } from "@tanstack/react-query";
 import { findEmail, loginService } from "@/lib/utils";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useToast } from "@/components/ui/use-toast";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "@/contexts/UserContextProvider";
 
-type loginprops={
-    fn?:()=>void;
-}
-export function Login(pros?:loginprops) {
-    const [step, setStep] = useState(1);
-    const [showDialog, setshowDialog] = useState(true);
-    const { toast } = useToast();
-    const { saveUser } = useContext(UserContext);
-    const navigate = useNavigate();
-    const { mutateAsync: checkEmailExistence, isPending: checkEmailExistencePending } = useMutation({
-        mutationFn: findEmail,
-    });
-    const { mutateAsync: loginServiceFn, isPending: loginServicePending } = useMutation({
-        mutationFn: loginService,
-    });
-    const form = useForm<SignIn>({
-        mode: 'onChange',
-        resolver: zodResolver(SignInSchema)
+type loginprops = {
+  fn?: () => void;
+};
+export function Login(pros?: loginprops) {
+  const [step, setStep] = useState(1);
+  const [showDialog, setshowDialog] = useState(true);
+  const { toast } = useToast();
+  const { saveUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  const {
+    mutateAsync: checkEmailExistence,
+    isPending: checkEmailExistencePending,
+  } = useMutation({
+    mutationFn: findEmail,
+  });
+  const {
+    mutateAsync: loginServiceFn,
+    isPending: loginServicePending,
+  } = useMutation({
+    mutationFn: loginService,
+  });
+  const form = useForm<SignIn>({
+    mode: "onChange",
+    resolver: zodResolver(SignInSchema),
+  });
+  function closeDialog(): void {
+    navigate(-1);
+    setshowDialog(false);
+  }
+  async function incrementStep(): Promise<void> {
+    if (step == 1) {
+      const res = await checkEmailExistence(form.getValues("email"));
+      if (!res?.available) {
+        toast({
+          description: "Sorry,we couldn't find your account",
+          variant: "secondary",
+          duration: 2000,
+        });
+        if (pros?.fn) pros?.fn();
+        return;
+      }
     }
-    );
-    function closeDialog(): void {
-        navigate(-1)
-        setshowDialog(false);
+    setStep(step + 1);
+  }
+  const onSubmit = async (data: SignIn) => {
+    const res = await loginServiceFn(data);
+    if (res) {
+      form.reset();
+      saveUser(res.user, res.token);
+      navigate("/settings");
+    } else {
+      toast({
+        description: "wrong password or email",
+        variant: "secondary",
+        duration: 2000,
+      });
+      return;
     }
-    async function incrementStep(): Promise<void> {
-        if (step == 1) {
-            const res = await checkEmailExistence(form.getValues("email"));
-            if (!res?.available) {
-                toast({
-                    description: "Sorry,we couldn't find your account",
-                    variant: "secondary",
-                    duration: 2000,
-                })
-                if(pros?.fn)  pros?.fn() 
-                return;
-            }
-        }
-        setStep(step + 1);
-    }
-    const onSubmit = async (data: SignIn) => {
-        const res = await loginServiceFn(data);
-        if (res) {
-            form.reset();
-            saveUser(res.user,res.token);
-            navigate("/settings");
-        }
-        else {
-            toast({
-                description: "wrong password or email",
-                variant: "secondary",
-                duration: 2000,
-            })
-            return;
-        }
-    }
-    return (
-        <>
-            <form onSubmit={form.handleSubmit(onSubmit)} >
-                <PopUpContainer className="flex flex-col" show={showDialog} showLogo={true} headerButton={HeaderButton.close} headerFunction={closeDialog} >
-                    {(checkEmailExistencePending || loginServicePending) && (
-                        <>
-                            <Skeleton className="mt-5 w-full h-[170px]" />
-                            <Skeleton className="w-full h-[50px] mt-auto mb-16" />
-                        </>
-                    )}
-                    {(step === 1 && !checkEmailExistencePending) && (<LoginEmail  incrementStep={incrementStep} form={form} />)}
-                    {(step === 2 && !loginServicePending) && (<LoginPassword
-                        incrementStep={form.handleSubmit((data) => {
-                            onSubmit(data);
-                        })}
-                        form={form} />)}
-                </PopUpContainer>
-            </form>
-        </>
-    )
+  };
+  return (
+    <>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <PopUpContainer
+          className="flex flex-col"
+          show={showDialog}
+          showLogo={true}
+          headerButton={HeaderButton.close}
+          headerFunction={closeDialog}
+        >
+          {(checkEmailExistencePending || loginServicePending) && (
+            <>
+              <Skeleton className="mt-5 w-full h-[170px]" />
+              <Skeleton className="w-full h-[50px] mt-auto mb-16" />
+            </>
+          )}
+          {step === 1 && !checkEmailExistencePending && (
+            <LoginEmail incrementStep={incrementStep} form={form} />
+          )}
+          {step === 2 && !loginServicePending && (
+            <LoginPassword
+              incrementStep={form.handleSubmit((data) => {
+                onSubmit(data);
+              })}
+              form={form}
+            />
+          )}
+        </PopUpContainer>
+      </form>
+    </>
+  );
 }
