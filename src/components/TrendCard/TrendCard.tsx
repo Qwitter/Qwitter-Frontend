ede16a28a29f9445a21f9d6e31febdd0c7a75e97
yousef osama <yousefosama654@gmail.com>
2023-12-06 23:12:49 +0200

    mute<add func show more button in trendcard>

--- src/components/TrendCard/TrendCard.tsx
+++ src/components/TrendCard/TrendCard.tsx
@@ -1,6 +1,8 @@
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { TrendList } from "../TrendList/TrendList";
+import { useNavigate } from "react-router-dom";
 export function TrendCard() {
+  const navigate = useNavigate();
   return (
     <>
       <Card className="bg-[#16181c] w-full  border-none">
@@ -9,7 +11,14 @@ export function TrendCard() {
         </CardHeader>
         <TrendList isCard={true} />
         <CardFooter className="hover:cursor-pointer mt-3 pt-3 hover:bg-light-gray rounded-br-lg rounded-bl-lg">
-          <span className="text-secondary">show more</span>
+          <span
+            className="text-secondary"
+            onClick={() => {
+              navigate("/Explore");
+            }}
+          >
+            show more
+          </span>
         </CardFooter>
       </Card>
     </>
