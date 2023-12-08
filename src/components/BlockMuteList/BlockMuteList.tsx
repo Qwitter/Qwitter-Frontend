efaff9a9c1a65f305abfc9dd2fe4e0ca4c927aa0
yousef osama <yousefosama654@gmail.com>
2023-12-08 17:34:10 +0200

    PI<fix bug and add BlockMuteListProps >

--- src/components/BlockMuteList/BlockMuteList.tsx
+++ src/components/BlockMuteList/BlockMuteList.tsx
@@ -1,18 +1,15 @@
 import { GetBlockedService, GetMutedService } from "@/lib/utils";
 import { OptionsHeader } from "../OptionsHeader/OptionsHeader";
 import { UsersList } from "../UsersList/UsersList";
+import { BlockMuteListProps } from "./BlockMuteListProps";
 
-export type BlockMuteListProps = {
-  headername: string;
-  service: string;
-};
 export function BlockMuteList({ headername, service }: BlockMuteListProps) {
   return (
     <>
       <div className=" w-full h-full border-r border-primary border-opacity-30 mb-20">
         <OptionsHeader header={headername} />
         <UsersList
-          listType={"BlockList"}
+          listType={service == "BlockList" ? "BlockList" : "MuteList"}
           showDesc={true}
           getusers={
             service == "BlockList" ? GetBlockedService : GetMutedService
